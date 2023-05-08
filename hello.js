133398e0b6ab06228c4807cd13d806a7e717f0f2
renukaghumare <ghumarerenuka@gmail.com>
2023-05-08 22:03:12 +0530

    comment
    comment

---
+++ hello.js
@@ -0,0 +1,20 @@
+
+
+
+
+console.log("Renuka Ghumare..");
+
+var firstName = "Gajanan";
+console.log(firstName);
+
+var age; // Variable Declaration 
+age = "22";  // Variable Initialization
+console.log(age);
+
+var holder; // Variable Declaration
+console.log(holder); // undefined
+holder = "My Spec"; // Variable Initialization
+console.log(holder);
+
+holder = "Sachin Spec"; // Variable update
+console.log(holder); //
