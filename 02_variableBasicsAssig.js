e68539cf39a945198b9735c9f3d23a6a0e23fcf3
renukaghumare <ghumarerenuka@gmail.com>
2023-05-12 23:15:14 +0530

    string implement
    string implement

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,9 @@
+// 1.Data types Number
+value of var->;
+console.log("Address: ", address);
+console.log("type of variable address is", typeof address);
+
+// 2. null
+var pin = null;
+console.log("Value of variable -> pin: ", pin);
+console.log('Type of variable pin is:', typeof pin);
---
+++ 04_stringAssignment.js
@@ -0,0 +1,27 @@
+console.log("String Basic Assignment");
+
+function stringBasics(){
+    console.log("My Dream Company is Mahagenco");
+}
+
+stringBasics();
+
+var hobby1="Playing Games";
+var hobby2="Cooking";
+var hobby3="Dancing";
+console.log("My hobbies are",hobby1,hobby2,hobby3);
+var hobby1Length=hobby1.length;
+var hobby2Length=hobby2.length;
+var hobby3Length=hobby3.length;
+
+var sum=hobby1Length+hobby2Length+hobby3Length;
+console.log("sum of total characters are in hobbies",sum);
+
+
+
+
+
+
+
+
+
