a0f6824deadb0959f75aa24072b3c3c7b3bcc698
renukaghumare <ghumarerenuka@gmail.com>
2023-05-15 22:00:51 +0530

    string handson implemented
    string handson implemented

--- 04_stringHandson.js
+++ 04_stringHandson.js
@@ -1,16 +1,36 @@
 function stringHandsOn(params) {
-    var string= " Hey you are doing good , keep it up" ;
-    console.log( " length After given String :"$ {lengthstring});
+  var string = "   Hey you are doing good , keep it up  ";
+  console.log(` is a string : ${string}`);
+  var stringlength = string.length;
+  console.log(` String length: ${string} : ${stringlength}`);
+  console.log("---step 3-----");
+  var trimmedstring = string.trim();
+  var lengthAfterTrim = trimmedstring.length;
+  console.log(` After Trim string is : ${trimmedstring}`);
+  console.log(` length After Trim string : ${lengthAfterTrim}`);
 }
 stringHandsOn();
-var string ="Hey you are doing good , keep it up";
-var charAtZeroIndex=string.charAt(0);
-var charAtLastIndex=string.charAt(string.length-1)
-console.log( firstandlastcharofstringtrim: $ { charAtZeroIndex},${charAtLastIndex});
 
-var searchResult=string.search("good");
-console.log( searchresultforgood: $ {searchResult} );
+var string = "Hey you are doing good , keep it up";
+var charAtZeroIndex = string.charAt(0);
+var charAtLastIndex = string.charAt(string.length - 1);
+console.log(
+  ` fist And last char of string trim : ${charAtZeroIndex} , ${charAtLastIndex}`);
 
+var searchResult = string.search("good");
+console.log(`Search Result for good : ${searchResult} `);
 
+var sliceResult = string.slice(17, 22);
+console.log(`slice Result:${sliceResult} `);
 
+var sliceResult = string.slice(22, 35);
+console.log(`slice Result After 22:${sliceResult} `);
 
+var result = string.endsWith("up");
+console.log(` Is ${string} :  string ends with character "up" : ${result}`);
+
+var result = string.startsWith("Hey");
+console.log(` Is ${string} :  string start with character "Hey" : ${result}`);
+
+var stringlength = string.length;
+console.log(` String: ${string} :  lenght of string:  ${stringlength}`);
