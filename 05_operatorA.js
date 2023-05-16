e2f90aeafa52b03fe41f6d13bf124c8bcad03dd6
renukaghumare <ghumarerenuka@gmail.com>
2023-05-16 23:29:17 +0530

    comment
    comment

---
+++ 05_operatorA.js
@@ -0,0 +1,24 @@
+console.log(" 1. The Square of length of word ");
+
+console.log("==============================================");
+
+
+function squareOfWordLength(string) {
+  
+}
+squareOfWordLength("Java Script");
+squareOfWordLength("Google Chrome");
+squareOfWordLength("Developer Smart");
+
+
+function Given a string(params) {
+    var string="I am Angular Developer";
+    var lengthstr;
+    var result = str.split ();
+    var totalwords=result.length;
+
+    
+}
+
+
+
