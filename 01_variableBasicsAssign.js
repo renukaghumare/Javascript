8f7451df6ff7ea8f5ea9a91849606b25ad764a37
renukaghumare <ghumarerenuka@gmail.com>
2023-05-28 14:40:44 +0530

    comment
    comment

---
+++ 01_variableBasicsAssign.js
@@ -0,0 +1,67 @@
+
+console.log("******1.Number*****");
+
+var pincode = 411056;
+console.log("pincode", pincode);
+var typeofpincode = typeof pincode;
+console.log(typeofpincode);
+
+var mobilenumber = 1234567890;
+console.log("moblienumber",mobilenumber);
+var typeofmobilenumber = typeof mobilenumber;
+console.log(typeofmobilenumber);
+
+var cash = 10000;
+console.log("cash", cash);
+var typeofcash = typeof cash;
+console.log(typeofcash);
+
+console.log("******2.String******");
+
+var Name =" Renuka";
+var typeofName = typeof Name;
+console.log(`Name is Renuka`);
+console.log(typeofName);
+
+
+var City =" Pune";
+var typeofCity = typeof City;
+console.log(`City  Pune`);
+console.log(typeofCity);
+
+var CollegeName="PCCOE ";
+var typeofCollegeName= typeof CollegeName;
+console.log(`College Name  PCCOE`);
+console.log(typeofCollegeName);
+
+
+console.log("***3.Boolean******");
+
+
+
+
+var isMarried = true;
+
+var typeOfIsMarried = typeof isMarried;
+
+console.log("Type of isMarried:", typeOfIsMarried);
+
+var isSoftwareEngineer = true;
+
+var isWorking = true;
+
+var isBachelor = false; 
+
+console.log(isBachelor);
+
+var typeOfIsMarried = typeof isMarried;
+
+console.log("Type of isMarried:", typeOfIsMarried);
+console.log("type of issoftware", typeof isSoftwareEngineer);
+console.log("type of isWorking", typeof isWorking);
+
+console.log("------------#Differance between null and undefined------------------- ");
+
+console.log("1.Null Variable:-A Null value means that there is no value or empty.");
+
+console.log("2.Undefined:- When we declare the variable and doesnt intilize its value. i.e undefined value.");
