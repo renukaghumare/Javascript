1197b6e4e807ec4804043843e6eb55578e3f8974
renukaghumare <ghumarerenuka@gmail.com>
2023-05-28 14:33:52 +0530

    While Assignment Implemented
    While Assignment Implemented

---
+++ 06_whileAssignment01.js
@@ -0,0 +1,74 @@
+
+console.log("WAP to Print Numbers from 5 to 15 by increment by 1");
+
+// WAP to print number from 5 to 15
+let i=5; // initialization i = 5 6 7 
+while (i<=15) { // condition 0<=15
+   console.log(i); // 5 6 7 
+   i = i + 1;
+}
+
+console.log("WAP to Print Numbers from 5 to 15 by decrement  by 1");
+
+// WAP to print number from 50 to 40
+let k = 50; 
+while (k>=40) {
+   console.log(k);
+   k--;
+}
+
+
+console.log("WAP to Print first 15 Odd numbers");
+
+let j=5;  // initialization
+while (j<=15) {  // condition
+   if (j%2 !==0) {
+      console.log(j);
+   }
+   j++; // update expression
+}
+
+console.log("WAP to Print first 10 even numbers");
+
+let n=0;  // initialization
+while (n<=10) {  // condition
+   if (n%2==0) {
+      console.log(n);
+   }
+   n++; // update expression
+}
+
+
+console.log("WAP to Print table of 5");
+
+// WAP to print table of 5to 50
+let a=5;  // initialization
+while (a<=50) {  // condition
+   if (a%5==0) {
+      console.log(a);
+   }
+   a++; // update expression
+}
+
+
+console.log("WAP to Print table of 10");
+
+// WAP to print table of 10 to 100
+let b=10;  // initialization
+while (b<=100) {  // condition
+   if (b%10==0) {
+      console.log(b);
+   }
+   b++; // update expression
+}
+
+console.log("WAP to Print table of 10 ");
+
+
+let c=100;  // initialization
+while (c<=10) {  // condition
+   if (c%100==0) {
+      console.log(b);
+   }
+   c++; // update expression
+}
